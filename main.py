29117f360f82740308bc7a31cc73fd8100336e93
romain.buret <romainburet14@gmail.com>
2018-05-23 14:34:23 +0200

    base projet

--- main.py
+++ main.py
@@ -52,7 +52,6 @@ def showWlan(event):
 # Frame
 F_choice = Frame(root, bg="white", borderwidth=2, relief=FLAT)
 F_change = Frame(F_choice, borderwidth=2, relief=GROOVE)
-F_create = Frame(F_choice, borderwidth=2, relief=SUNKEN)
 
 # Spinbox
 
@@ -74,13 +73,13 @@ L_titre.pack()
 L_ethCard.pack()
 B_openFile.bind("<Button-1>",getDataIP)
 B_openFile.pack()
+Li_file.bind("<Double-Button-1>",changeIP)
 Li_file.pack()
 F_change.pack(side=LEFT, padx=30, pady=30)
 B_change.bind("<Button-1>", changeIP)
 B_change.pack()
 B_raz.bind("<Button-1>", razIP)
 B_raz.pack()
-F_create.pack(side=RIGHT, padx=30, pady=30)
 F_choice.pack()
 B_close.pack()
 root.mainloop()
